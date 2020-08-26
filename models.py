9125c8aca53da9be48da6a7c93db1de3304895b5
Luciano Gomes Vieira dos Anjos <62733659+lucianoanjos02@users.noreply.github.com>
2020-08-26 12:56:33 -0300

    Adição da Constraint Nullable à classe Usuario
    Constraint nullable incluída aos atributos da classe Usuario.
    
    Constraint: nullable=False

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ from sqlalchemy import Column, String, Integer
 class Usuario(Base):
     __tablename__ = 'TUsuario'
     id_usuario = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(25))
-    sobrenome = Column(String(25)) 
-    email = Column(String(150), unique=True)
-    login = Column(String(20), unique=True)
-    senha = Column(String(10))
+    nome = Column(String(25), nullable=False)
+    sobrenome = Column(String(25), nullable=False) 
+    email = Column(String(150), unique=True, nullable=False)
+    login = Column(String(20), unique=True, nullable=False)
+    senha = Column(String(10), nullable=False)
 
     def __init__(self, nome, sobrenome, email, login, senha):
         self.nome = nome
