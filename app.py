36634f72e13696753fac58b5e49c6ecc079f4c4a
Luciano Gomes Vieira dos Anjos <62733659+lucianoanjos02@users.noreply.github.com>
2020-08-27 18:03:06 -0300

    View e Controle de Login (Concluído)
    View login.html concluída.
    
    - Implementado o Flask-Bootstrap na aplicação para estilização das páginas;
    
    -Implementada a autenticação e sessão de usuário para logar e se manter logado na aplicação;
    
    - Implementado o decorator @login_required do Flask-Login para restringir o acesso às páginas da aplicação apenas à usuários devidamente logados e autenticados;
    
    - Implementado a folha de estilo CSS login.css para melhoria no estilo da view junto com o Flask-Bootstrap;
    
    - Implementada a rota de logout da aplicação, com o Flask-Login.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_login import LoginManager, login_required, login_user, logout_user, current_user
+from flask_bootstrap import Bootstrap
 from database import db_session
 from forms import LoginForm
 from dao import UsuarioDAO
@@ -7,36 +8,79 @@ import os
 import binascii
 
 
-app = Flask(__name__)
+app = Flask(__name__) #INSTANCIA DO PROJETO FLASK
 
+Bootstrap(app) #INSTANCIANDO O PROJETO FLASK (APP) NO FLASK-BOOTSTRAP
+
+#CONFIGURAÇÕES DO FLASK-LOGIN
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+login_manager.login_message = "Por favor, faça o login para acessar o sistema!"
 
+#INSTANCIA DO UsuarioDAO
 usuario_dao = UsuarioDAO(db_session)
 
+#CONFIGURAÇÕES DA APP
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = binascii.hexlify(os.urandom(24))
 
 
 @login_manager.user_loader
 def load_user(id_usuario):
+    '''
+    CLASSE IMPLEMENTADA PARA CARREGAR O USUÁRIO LOGADO PELO FLASK-LOGIN
+
+    @autor: Luciano Gomes Vieira dos Anjos -
+    @data: 27/08/2020 -
+    @versao: 1.0.0
+    '''
     return usuario_dao.get_id_usuario(id_usuario)
 
 
 @app.route('/')
 def dashboard_redirect():
+    '''
+    ROTA DE ACESSO PARA REDIRECIONAMENTO À PÁGINA DE LOGIN, CASO O
+    USUÁRIO ACESSE A APLICAÇÃO PELA ROTA APENAS COM '/' NO LINK
+
+    @autor: Luciano Gomes Vieira dos Anjos -
+    @data: 27/08/2020 -
+    @URL: http://localhost:5000/ - 
+    @versao: 1.0.0
+    '''
     return redirect(url_for('login'))
 
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
+    '''
+    ROTA DE ACESSO PARA O DASHBOARD, PÁGINA PRINCIPAL DA APLICAÇÃO
+
+    - IMPLEMENTADO O DECORATOR @login_required PARA PREVINIR O ACESSO
+    DE USUÁRIOS QUE NÃO ESTÃO LOGADOS/AUTENTICADOS
+
+    @autor: Luciano Gomes Vieira dos Anjos -
+    @data: 27/08/2020 -
+    @URL: http://localhost:5000/dashboard - 
+    @versao: 1.0.0
+    '''
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    ROTA DE ACESSO PARA A PÁGINA DE LOGIN DA APLICAÇÃO
+
+    - FORMULÁRIO DE LOGIN IMPLEMENTADO PELO Flask WTF
+
+    @autor: Luciano Gomes Vieira dos Anjos -
+    @data: 27/08/2020 -
+    @URL: http://localhost:5000/login - 
+    @versao: 1.0.0
+    '''
     form = LoginForm()
     if form.validate_on_submit():
         usuario = usuario_dao.get_login_usuario(form.login.data)
@@ -48,5 +92,23 @@ def login():
     return render_template('login.html', form=form)
 
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    '''
+    ROTA UTILIZADA PARA REALIZAR O LOGOUT DO USUÁRIO DA APLICAÇÃO
+
+    - IMPLEMENTADO O MÉTODO logout_user DO PRÓPRIO FLASK-LOGIN
+
+    @autor: Luciano Gomes Vieira dos Anjos -
+    @data: 27/08/2020 -
+    @URL: http://localhost:5000/login - 
+    @versao: 1.0.0
+    '''
+    logout_user()
+    return redirect(url_for('login'))
+
+
+#BLOCO DE INICIALIZAÇÃO DA APLICAÇÃO IMPEDE QUE 
+#A APP SEJA INICIALIZADA CASO IMPORTADA EM OUTRO MODULO
 if __name__ == '__main__':
     app.run(host='localhost', port='5000', debug=True)
