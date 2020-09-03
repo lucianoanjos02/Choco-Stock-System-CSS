f984266d9b4b4592dc924c3d436230c57e847ca8
Luciano Gomes Vieira dos Anjos <62733659+lucianoanjos02@users.noreply.github.com>
2020-09-03 14:17:02 -0300

    Revert "Correção nas Relações das Classes/Tabelas e Criação das Classes Associativas"
    This reverts commit 2a5353c6080618e23eedbb1ea414abca2a10a8be.

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from database import Base
 from flask_login import UserMixin
 from sqlalchemy.orm import relationship, backref
-from sqlalchemy import Column, String, Integer, Date, ForeignKey, Float
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
 
 class Usuario(Base, UserMixin):
     '''
@@ -21,7 +21,6 @@ class Usuario(Base, UserMixin):
     senha = Column(String(10), nullable=False)
     id_permissao = Column(Integer, ForeignKey('TPermissao.id_permissao'))
     role = relationship('Permissao', backref=backref('TUsuario', lazy='dynamic'))
-    loja = relationship("UsuarioLoja", backref=backref('TUsuario', lazy='dynamic'))
 
     def __init__(self, nome, sobrenome, email, login, senha):
         self.nome = nome
@@ -69,7 +68,7 @@ class Permissao(Base):
 class Loja(Base):
     '''
 
-    CLASSE LOJA - MAPEIA TABELA TLoja NO BANCO DE DADOS
+    CLASSE LOJA - MAPEIA TABELA Tloja NO BANCO DE DADOS
 
     @autor: Gabriel Oliveira Gonçalves -
     @data: 29/08/2020 -
@@ -80,46 +79,14 @@ class Loja(Base):
     razao_social = Column(String(100), nullable=False, unique=True)
     nome_fantasia = Column(String(100), nullable=False, unique=True)
     cnpj = Column(String(11), nullable=False, unique=True)
-    logradouro = Column(String(100), nullable=False)
-    numero_logradouro = Column(String(10), nullable=False)
-    cep = Column(String(8), nullable=False)
+    endereco = Column(String(100), nullable=False)
     inscricao_estadual = Column(String(20), nullable=False, unique=True)
     email = Column(String(50), nullable=False, unique=True)
-    usuarios = relationship('UsuarioLoja', backref=backref('TLoja', lazy='dynamic'))
-    estoque = relationship('Estoque', backref=backref('TLoja', lazy='dymanic'))
+    id_usuario = Column(Integer, ForeignKey('TUsuraio.id_usuario'))
+    usuario = relationship('Usuario', backref=backref('TLoja', lazy='dynamic'))
 
-    def __init__(self, razao_social, nome_fantasia, cnpj, logradouro, numero_logradouro, cep, inscricao_estadual, email, usuario_loja):
-        self.id_loja = id_loja
-        self.razao_social = razao_social
-        self.nome_fantasia = nome_fantasia
-        self.cnpj = cnpj
-        self.logradouro = logradouro
-        self.numero_logradouro = numero_logradouro
-        self.cep = cep
-        self.inscricao_estadual = inscricao_estadual
-        self.email = email
-        self.id_usuario = id_usuario
-        self.usuario = usuario
-
-
-class UsuarioLoja(Base):
-    '''
-
-    CLASSE UsuarioLoja - MAPEIA TABELA TUsuario_Loja NO BANCO DE DADOS 
-    QUE FAZ A ASSOCIAÇÃO ENTRE Usuario E Loja
-  
-    @autor: Luciano Gomes Vieira dos Anjos -
-    @data: 02/09/2020 -
-    @versao: 1.0.0
-    '''
-    __tablename__ = 'TUsuario_Loja'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    id_loja = Column(Integer, ForeignKey('TLoja.id_loja'))
-    id_usuario = Column(Integer, ForeignKey('TUsuario.id_usuario'))
-
-    def __init__(self, id_loja, id_usuario):
-        self.id_loja = id_loja
-        self.id_usuario = id_usuario
+    def __repr__(self):
+        return f'<Loja:{self.razao_social, self.nome_fantasia, self.cnpj, self.endereco, self.inscricao_estadual, self.cnpj}>'
 
 
 class Estoque(Base):
@@ -133,21 +100,16 @@ class Estoque(Base):
     '''
     __tablename__ = 'TEstoque'
     id_estoque = Column(Integer, primary_key=True, autoincrement=True)
-    codigo_lote = Column(String(10), nullable=False)
+    numero_lote = Column(String(10), nullable=False)
     quantidade = Column(Integer, nullable=False)
     data_fabricacao = Column(Date, nullable=False)
     data_validade = Column(Date, nullable=False)
     total_item = Column(Integer, nullable=False)
     id_loja = Column(Integer, ForeignKey('TLoja.id_loja'))
-    produtos = relationship("EstoqueProduto", backref=backref('TEstoque', lazy='dynamic'))
+    loja = relationship('Loja', backref=backref('TEstoque', lazy='dymanic'))
 
-    def __init__(self, numero_lote, quantidade, data_fabricacao, data_validade, total_item):
-        self.numero_lote = numero_lote
-        self.quantidade = quantidade
-        self.data_fabricacao = data_fabricacao
-        self.data_validade = data_validade
-        self.total_item = total_item
-
+    def __repr__(self):
+        return f'<Estoque:{self.numero_lote, self.quantidade, self.data_fabricacao, self.data_validade, self.total_item}>'
 
 class Produto(Base):
     '''
@@ -158,47 +120,20 @@ class Produto(Base):
     @versao: 1.0.0
     '''
     __tablename__ = 'TProduto'
-    id_produto  = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
+    id_produto = Column(Integer, primary_key=True, autoincrement=True)
     codigo_barras = Column(String(100), nullable=False)
-    preco = Column(Float, nullable=False)
-    id_tipo = Column(Integer, ForeignKey('TTipo_Produto.id'))
+    nome_produto = Column(String(100), nullable=False)
+    preco_produto = Column(Integer, nullable=False)
     id_loja = Column(Integer, ForeignKey('TLoja.id_loja'))
     id_estoque = Column(Integer, ForeignKey('TEstoque.id_estoque'))
-    tipo = relationship('TipoProduto', backref=backref('TProduto', lazy='dynamic'))
-    estoque = relationship('EstoqueProduto', backref=backref('TProduto', lazy='dynamic'))
-    kit = relationship('Kit', backref=backref('TProduto', lazy='dynamic'))
+    store = relationship('Loja', backref=backref('TProduto', lazy='dynamic'))
+    estoque = relationship('Estoque', backref=backref('TProduto', lazy='dynamic'))
 
-    def __init__(self, nome_produto, codigo_barras, preco_produto, id_tipo, id_loja, id_estoque):
-        self.nome_produto = nome_produto
-        self.codigo_barras = codigo_barras
-        self.preco_produto = preco_produto
-        self.id_tipo = id_tipo
-        self.id_loja = id_loja
-        self.id_estoque = id_estoque
+    def __repr__(self):
+        return f'<Produto:{self.codigo_barras, self.nome_produto, self.preco_produto}>'
 
 
-class EstoqueProduto(Base):
-    '''
-
-    CLASSE EstoqueProduto - MAPEIA TABELA TEstoque_Produto NO BANCO DE DADOS
-    QUE FAZ A ASSOCIAÇÃO ENTRE Estoque E Produto
-  
-    @autor: Luciano Gomes Vieira dos Anjos -
-    @data: 02/09/2020 -
-    @versao: 1.0.0
-    '''
-    __tablename__ = 'TEstoque_Produto'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    id_estoque = Column(Integer, ForeignKey('TEstoque.id_estoque'))
-    id_produto = Column(Integer, ForeignKey('TProduto.id_produto'))
-
-    def __init__(self, id_loja, id_produto):
-        self.id_loja = id_loja
-        self.id_produto = id_produto
-
-
-class TipoProduto(Base):
+class Tipo_Produto(Base):
     '''
 
     CLASSE TIPO_PRODUTO - MAPEIA TABELA TTp_Produto NO BANCO DE DADOS
@@ -207,12 +142,14 @@ class TipoProduto(Base):
     @data: 29/08/2020 -
     @versao: 1.0.0
     '''
-    __tablename__ = 'TTipo_Produto'
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    tipo = Column(String(100), nullable=False)
+    __tablename__ = 'TTp_Produto'
+    id_tp_produto = Column(Integer, primary_key=True, autoincrement=True)
+    nome_tp_produto = Column(String(100), nullable=False)
+    id_produto = Column(Integer, ForeignKey('TProduto.id_produto'))
+    produto = relationship('Produto', backref=backref('TTp_Produto', lazy='dynamic'))
 
-    def __init__(self, tipo):
-        self.tipo = tipo
+    def __repr__(self):
+        return f'<Tipo_Produto:{self.nome_tp_produto}>'
     
 
 class Kit(Base):
@@ -226,15 +163,12 @@ class Kit(Base):
     '''
     __tablename__ = 'TKit'
     id_kit = Column(Integer, primary_key=True, autoincrement=True)
-    nome = Column(String(100), nullable=False)
-    quantidade = Column(Integer, nullable=False)
-    preco = Column(Float, nullable=False)
-    data_validade = Column(Date, nullable=False)
+    nome_kit = Column(String(100), nullable=False)
+    qtd_kit = Column(Integer, nullable=False)
+    preco_kit = Column(Integer, nullable=False)
+    validade_kit = Column(Date, nullable=False)
     id_produto = Column(Integer, ForeignKey('TProduto.id_produto'))
+    produto_2 = relationship('Produto', backref=backref('TKit', lazy='dynamic'))
 
-    def __init__(self, nome, quantidade, preco, data_validade, id_produto):
-        self.nome = nome
-        self.quantidade = quantidade
-        self.preco = preco
-        self.data_validade = data_validade
-        self.id_produto = id_produto
+    def __repr__(self):
+        return f'<Kit:{self.nome_kit, self.qtd_kit, self.preco_kit, self.validade_kit}>'
