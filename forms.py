ce3328e55e3d58af5ad61da992dde3b1c9a2984c
gonca-ads <53906512+gonca-ads@users.noreply.github.com>
2020-09-07 21:15:51 -0300

    Cadastro de Usuário
    View Cadastro de Usuário

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SelectField
 from wtforms.validators import Email, Length, InputRequired
+from dao import PermissaoDAO
+from database import db_session
 
+permissao_dao = PermissaoDAO(db_session)
 
 class LoginForm(FlaskForm):
     '''
@@ -13,3 +16,15 @@ class LoginForm(FlaskForm):
     '''
     login = StringField('Login', validators=[InputRequired(), Length(max=50)])
     senha = PasswordField('Senha', validators=[InputRequired(), Length(max=10)])
+
+
+class CadastroUsuarioForm(FlaskForm):
+    '''
+
+    '''
+    nome = StringField('Nome', validators=[InputRequired(), Length(max=25)])
+    sobrenome = StringField('Sobrenome', validators=[InputRequired(), Length(max=25)])
+    email = StringField('Email', validators=[InputRequired(), Email(message="E-mail Inválido"), Length(max=150)])
+    login = StringField('Login', validators=[InputRequired(), Length(max=20)])
+    senha = StringField('Senha', validators=[InputRequired(), Length(max=10)])
+    permissao = SelectField('Permissão', choices=permissao_dao.get_permissoes()) 
